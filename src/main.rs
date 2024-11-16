3e6c2c0d4e0c3c1f343c824ca7b55efc1060f3f0
R. G. Esteves <rgesteves@gmail.com>
2024-11-16 05:22:42 +0000

    Simple function using ranges

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    print!("Hello, world, using exit code!\n");
+    print!("Hello, world, calling a function\n");
+    gen_seq_list();
+    print!("\nDone!\n");
     std::process::exit(0);
 }
+
+fn gen_seq_list() {
+    for i in 0..12 { println!("{}", i); }
+}
+/* 
+use std::iter::range;
+
+fn gen_seq_list() {
+    let numbers: Vec<u32> = range(1, 11).collect();
+    println!("{:?}", numbers); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}
+*/
 
 #[test]
 fn multiply_test() {
