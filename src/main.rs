62cec70180c536964b7ed262ff244d7152a66293
R. G. Esteves <rgesteves@gmail.com>
2024-11-16 18:33:07 +0000

    mutability, macros

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     print!("Hello, world, calling a function\n");
     gen_seq_list();
+    gen_seq_list_imperative();
     print!("\nDone!\n");
     std::process::exit(0);
 }
@@ -16,6 +17,19 @@ fn gen_seq_list() {
     println!("{:?}", numbers); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
 */
+
+fn gen_seq_list_imperative() {
+    let mut newvec = Vec::new();
+    let mut counter = 1;
+    loop {
+        newvec.push(counter);
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    }
+    println!("{newvec:?}");
+}
 
 #[test]
 fn multiply_test() {
